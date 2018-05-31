f6f8441b51c527fdeabde851734dd1f04aa944f2
wilgustavo <wilgustavo@gmail.com>
2018-05-31 18:57:10 -0300

    quitar body-parser

--- functions/src/app.ts
+++ functions/src/app.ts
@@ -1,6 +1,5 @@
 import * as admin from 'firebase-admin';
 import * as express from 'express';
-import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import routes from './routes';
 
@@ -8,9 +7,6 @@ admin.initializeApp();
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
 
 routes(app);
 
