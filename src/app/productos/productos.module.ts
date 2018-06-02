59d55f4223407adae2fb6352aeb045506c5ce0c1
wilgustavo <wilgustavo@gmail.com>
2018-06-02 16:55:38 -0300

    Componente de formulario para ABM de productos

--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductosService } from './productos.service';
 import { ProductoListaComponent } from '../producto-lista/producto-lista.component';
+import { ProductoFormComponent } from './producto-form/producto-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule
   ],
-  declarations: [ProductoListaComponent],
+  declarations: [ProductoListaComponent, ProductoFormComponent],
   providers: [ProductosService],
   exports: [ProductoListaComponent]
 })
