77f593266c674afaae701cbbc53db978fcc1e751
wilgustavo <wilgustavo@gmail.com>
2018-06-02 18:09:54 -0300

    Formulario de producto

--- src/app/productos/producto-form/producto-form.component.ts
+++ src/app/productos/producto-form/producto-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-producto-form',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductoFormComponent implements OnInit {
 
-  constructor() { }
+  productoForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+  }
+
+  crearFormulario() {
+    this.productoForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      precio: [0.0, Validators.required],
+      estado: ['', Validators.required],
+      imagen: ''
+    });
+
   }
 
 }
--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ProductosService } from './productos.service';
 import { ProductoListaComponent } from './producto-lista/producto-lista.component';
 import { ProductoFormComponent } from './producto-form/producto-form.component';
@@ -8,10 +9,11 @@ import { ProductoFormComponent } from './producto-form/producto-form.component';
 @NgModule({
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   declarations: [ProductoListaComponent, ProductoFormComponent],
   providers: [ProductosService],
-  exports: [ProductoListaComponent]
+  exports: [ProductoListaComponent, ProductoFormComponent]
 })
 export class ProductosModule { }
