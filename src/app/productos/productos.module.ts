364be1acf67ef78c07fb53f95868d2f49124d40b
wilgustavo <wilgustavo@gmail.com>
2018-06-05 19:36:19 -0300

    Agregar formulario

--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { ProductosService } from './productos.service';
 import { ProductoListaComponent } from './producto-lista/producto-lista.component';
 import { ProductoFormComponent } from './producto-form/producto-form.component';
@@ -13,9 +16,12 @@ import { ProductoDialogComponent } from './producto-dialog/producto-dialog.compo
   imports: [
     CommonModule,
     HttpClientModule,
+    MatButtonModule,
     ReactiveFormsModule,
     MatListModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   declarations: [ProductoListaComponent,
                  ProductoFormComponent,
