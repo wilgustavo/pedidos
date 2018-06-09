dc54b8e301a9d50146656d554f051fc29dcbbd67
wilgustavo <wilgustavo@gmail.com>
2018-06-09 15:38:27 -0300

    Retornar mensajes de errores de validación

--- functions/src/controllers/producto.controller.ts
+++ functions/src/controllers/producto.controller.ts
@@ -6,14 +6,12 @@ export class ProductoController {
 
   constructor(private db: ProductoDB) { }
   
-  getProductos = (req: Request, res: Response) => {
+  getProductos = (req: Request, res: Response, next: NextFunction) => {
     this.db.getProductos()
       .then(lista => {
         res.json(lista);
       })
-      .catch(err => { 
-        res.status(500).send(err); 
-      });
+      .catch(err => next(err));
   }
 
   getProducto = (req: Request, res: Response, next: NextFunction) => {
@@ -24,33 +22,27 @@ export class ProductoController {
       .catch(err => next(err));
   }
 
-  crearProducto = (req: Request, res: Response) => {
+  crearProducto = (req: Request, res: Response, next: NextFunction) => {
     this.db.crearProducto(req.body)
       .then(doc => {
         res.json(doc);
       })
-      .catch(err => {
-        res.status(500).send(err);
-      });
+      .catch(err => next(err));
   }
 
-  grabarProducto = (req: Request, res: Response) => {
+  grabarProducto = (req: Request, res: Response, next: NextFunction) => {
     this.db.guardarProducto(Producto.addId(req.body, req.params.id))
       .then(() => {
         res.status(204).send();
       })
-      .catch(err => {
-        res.status(500).send(err);
-      });
+      .catch(err => next(err));
   }
 
-  borrarProducto = (req: Request, res: Response) => {
+  borrarProducto = (req: Request, res: Response, next: NextFunction) => {
     this.db.borrarProducto(req.params.id)
       .then(() => {
         res.status(204).send();
       })
-      .catch(err => {
-        res.status(500).send(err);
-      });
+      .catch(err => next(err));
   }
 } 
