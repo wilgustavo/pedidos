16e617ed986caad3ae92233c5a4c84d60518be33
wilgustavo <wilgustavo@gmail.com>
2018-07-07 19:16:26 -0300

    Editar item de la lista de productos

--- src/app/productos/producto-lista/producto-lista.component.ts
+++ src/app/productos/producto-lista/producto-lista.component.ts
@@ -1,30 +1,29 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ProductosStorage } from '../productos.storage';
-import { Producto } from 'functions/src/models/producto.model';
-import { Subscription, forkJoin } from 'rxjs';
-import { flatMap } from 'rxjs/operators';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ProductosStorage } from "../productos.storage";
+import { Producto } from "functions/src/models/producto.model";
+import { Subscription } from "rxjs";
+import { MatDialogConfig, MatDialog } from "@angular/material";
+import { ProductoDialogComponent } from "../producto-dialog/producto-dialog.component";
 
 class SeleccionProducto {
-  constructor(
-    public producto: Producto, 
-    public seleccionado: boolean
-  ) {}
+  constructor(public producto: Producto, public seleccionado: boolean) {}
 }
 
 @Component({
-  selector: 'app-producto-lista',
-  templateUrl: './producto-lista.component.html',
-  styleUrls: ['./producto-lista.component.css']
+  selector: "app-producto-lista",
+  templateUrl: "./producto-lista.component.html",
+  styleUrls: ["./producto-lista.component.css"]
 })
 export class ProductoListaComponent implements OnInit, OnDestroy {
-
   productos: SeleccionProducto[];
   seleccionados: number;
   onChange: Subscription;
   actualizando: boolean;
 
   constructor(
-    private productoStorage: ProductosStorage) { }
+    private productoStorage: ProductosStorage,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit() {
     this.actualizando = true;
@@ -47,7 +46,9 @@ export class ProductoListaComponent implements OnInit, OnDestroy {
   }
 
   getProductos() {
-    this.productos = this.productoStorage.getProductos().map(item => new SeleccionProducto(item, false));
+    this.productos = this.productoStorage
+      .getProductos()
+      .map(item => new SeleccionProducto(item, false));
     this.seleccionados = 0;
   }
 
@@ -73,4 +74,11 @@ export class ProductoListaComponent implements OnInit, OnDestroy {
     ids.forEach(id => this.productoStorage.borrarProducto(id).subscribe());
   }
 
+  editarProducto(item: SeleccionProducto) {
+    const config = new MatDialogConfig();
+    config.autoFocus = true;
+    config.data = item;
+    const ref = this.dialog.open(ProductoDialogComponent, config);
+    ref.afterClosed().subscribe();
+  }
 }
